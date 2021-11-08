e101db51bde8ac43e83a981bd7041a70307bc5cf
HaydenCrocker <crocker.hayden@gmail.com>
2021-11-08 00:06:57 -0500

    acquired Array for 8 pages worth of 20 results of movies

--- scripts.js
+++ scripts.js
@@ -3,63 +3,101 @@ const genreApp = {};
 
 genreApp.apiKey = '321a6d0985356d8f9304cc76a294aab3';
 genreApp.apiUrl = 'https://api.themoviedb.org/3/discover/movie';
+genreApp.apiURL2 = 'https://api.themoviedb.org/3/genre/movie/list';
+genreApp.arrayOfMovies = []
 
-genreApp.getGenres = () => {
-
-    const url = new URL(genreApp.apiUrl);
-    url.search = new URLSearchParams({
+genreApp.callApi = async function (url, page = 1) {
+    const address = new URL(url);
+    address.search = new URLSearchParams({
         api_key: genreApp.apiKey,
+        page,
+        // with_genres: '35'
     })
-
-    fetch(url)
+    await fetch(address)
         .then((response) => {
             return response.json();
         })
         .then((jsonResult) => {
-            console.log('it worked!', jsonResult);
-
-            genreApp.displayGenres(jsonResult);
+            // console.log(jsonResult);
+            if (jsonResult.results.length > 0) {
+                genreApp.arrayOfMovies.push(...jsonResult.results);
+                if (jsonResult.page !== 8) {
+                    genreApp.callApi(url, page + 1);
+                } 
+            }
         })
 }
 
-// When the user inputs a genre, connect the genre IDs to the filtered array to match
+genreApp.getGenres = () => {
+    genreApp.callApi(genreApp.apiUrl);
+    console.log(genreApp.arrayOfMovies);
+    // const url2 = new URL(genreApp.apiURL2);
+    // url2.search = new URLSearchParams({
+    //     api_key: genreApp.apiKey,
+    // })
+
+    // fetch(url2)
+    //     .then((response) => {
+    //         return response.json();
+    //     })
+    //     .then((jsonResult) => {
+    //         console.log(jsonResult);
+    //         genreApp.displayGenreIds(jsonResult);
+    //     })
+}
 
 genreApp.displayGenres = (arrayDataFromApi) => {
     
-    const userInput = arrayDataFromApi.results.genre_ids[0];
-    
-    const selectedGenre = userInput.filter((genre) => {
-        return genre === genre_ids[0];
-    });
-
-    const cardContainer = document.querySelector('.cardContainer');
-    // const movieCard = arrayDataFromApi.results;
-        const ulEl = document.createElement('ul');
-        const liEl = document.createElement('li');
-        const paragraph = document.createElement('p');
-        paragraph.innerHTML = `TITLE: ${arrayDataFromApi.results[0].original_title}`
-
-        liEl.appendChild(paragraph);
-        ulEl.appendChild(liEl);
-        cardContainer.appendChild(ulEl);
-}
-
-genreApp.init = () => {
-    genreApp.getGenres();
-}
-
-genreApp.init();
-// A page where the user is able to input a genre of movie and is met with movie reccomendations.
-
-// There will be a button for the user to submit their response
-
-// The button will listen for a click event that will create new elements that display the movie titles (with animation)
-
-// When a user searches for a specific genre, the page will populate with multiple movies that fit that genre
+    // if (userInput === genreID) {
+        //     // const cardContainer = document.querySelector('.cardContainer');
+        //     // // const movieCard = arrayDataFromApi.results;
+        //     //     const ulEl = document.createElement('ul');
+        //     //     const liEl = document.createElement('li');
+        //     //     const paragraph = document.createElement('p');
+        //     //     paragraph.innerHTML = `TITLE: ${arrayDataFromApi.results[0].original_title}`
+        // }
+        //     liEl.appendChild(paragraph);
+        //     ulEl.appendChild(liEl);
+        //     cardContainer.appendChild(ulEl);
+        
+        
+        const selectedGenre = arrayDataFromApi.results.filter((genre) => {
+            // console.log(genre.genre_ids);
+            
+        });
+        
+        
+        // const selectedGenre = userInput.filter((genre) => {
+            //     return genre === genre_ids[0];
+            // });
+            
+        }
+        
+        genreApp.displayGenreIds = (genresArray) => {
+            
+        }
+        
+        genreApp.init = () => {
+            const movieButton = document.querySelector('form').addEventListener('submit', function (e) {
+                e.preventDefault();
+                genreApp.getGenres();
+                const userInput = document.getElementById('genre').value;
+                console.log(userInput)
+            });
+        }
+        
+        genreApp.init();
+        // A page where the user is able to input a genre of movie and is met with movie reccomendations.
+        
+        // There will be a button for the user to submit their response
+        
+        // The button will listen for a click event that will create new elements that display the movie titles (with animation)
+        
+        // When a user searches for a specific genre, the page will populate with multiple movies that fit that genre
 
 // the page will reveal movie cards in stacked columns and rows. 
 
-// IF the user decides to search for another genre, the search bar will remain in its position at the top of the page.
+// If the user decides to search for another genre, the search bar will remain in its position at the top of the page.
 
 // Create an app object (genreApp)
 
